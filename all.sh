1f04fe68553b341e1ed5a698f6c935eccc6a3e7f
gelutz <gabriel.lutz5@gmail.com>
2026-01-06 13:28:54 -0300

    feat: updating projects

--- all.sh
+++ all.sh
@@ -1,1 +1,29 @@
-git submodule foreach  --recursive "$1"
+#!/usr/bin/env bash
+
+set -e
+
+IGNORE=""
+while getopts "i:" opt; do
+  case "$opt" in
+    i) IGNORE="$OPTARG" ;;
+    *) echo "Usage: $0 [-i submodule_name] <command>"; exit 1 ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if [ $# -eq 0 ]; then
+  echo "Error: no command provided"
+  exit 1
+fi
+
+CMD="$*"
+
+git submodule foreach --recursive '
+  if [ -n "'"$IGNORE"'" ] && \
+     { [ "$name" = "'"$IGNORE"'" ] || [[ "$path" == *"'"$IGNORE"'"* ]]; }; then
+    echo "Skipping submodule: $name"
+    exit 0
+  fi
+
+  echo "Running in $name"
+  '"$CMD"
